160930b962e7442af063815ab0eb6054428d0a77
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2026-04-09 18:51:04 +0300

    Simplify train.py usage

--- src/train.py
+++ src/train.py
@@ -15,22 +15,27 @@ import pickle
 import data_loader
 import network
 
-def train():
+def train(network_structure: list, epochs: int, mini_batch_size: int,
+          learning_rate: float, extract_parameters=True, testing=True):
     # load the training, validation and test data
     training_data, validation_data, test_data = data_loader.load_mnist()
 
     # network structure [input layer, hidden layer, ..., hidden layer, output layer]
-    net = network.Network([784, 30, 10])
+    net = network.Network(network_structure)
 
     # train with: training data, epochs, mini batch size, learning rate, testing data
-    net.stochastic_gradient_descent(training_data, 10, 30, 5, test_data)
+    if testing:
+        net.stochastic_gradient_descent(training_data, epochs, mini_batch_size, learning_rate, test_data)
+    else:
+        net.stochastic_gradient_descent(training_data, epochs, mini_batch_size, learning_rate)
 
     # extract biases and weights into a file
-    extract_parameters = True
+    extract_parameters = extract_parameters
     if extract_parameters:
         sizes, weights, biases = net.extract_parameters()
         with open("data/parameters.pkl", "wb") as f:
             pickle.dump((sizes, weights, biases), f)
 
 if __name__ == "__main__":
-    train()
+    train(network_structure=[784, 30, 10], epochs=10, mini_batch_size=30,
+          learning_rate=5, extract_parameters=False, testing=True)
