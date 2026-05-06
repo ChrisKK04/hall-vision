16ca9fe929f942c1f44ab5d8df1ab8ecfbc3a45b
Kristjan Kaasik <kristjankk25@gmail.com>
2026-05-06 21:12:39 +0300

    Modify and simplify plot.py to allow for parallel plot extraction

--- src/plot.py
+++ src/plot.py
@@ -28,7 +28,6 @@ class PlotNetwork(network.Network):
 
     def stochastic_gradient_descent(self, training_data, epochs, batch_size, training_rate, test_data):
         self.accuracy_initial = self.accuracy(test_data)
-        print(self.accuracy_initial)
         for epoch in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[end_index - batch_size:end_index]
@@ -36,40 +35,49 @@ class PlotNetwork(network.Network):
             for mini_batch in mini_batches:
                 self.update_parameters(mini_batch, training_rate)
             self.accuracies.append(self.accuracy(test_data))
-            print(self.accuracies)
     
     def accuracy(self, test_data):
         correct_count = [1 for sample in test_data if np.argmax(self.feedforward(sample[0])) == sample[1]]
         return len(correct_count)
 
-def extract_scatter(network_structure, epochs, mini_batch_size, learning_rate):
+def extract_plot(network_structure, epochs, mini_batch_size, learning_rate):
     net = PlotNetwork(network_structure)
     net.stochastic_gradient_descent(training_data, epochs, mini_batch_size, learning_rate, test_data)
     return (net.accuracy_initial, list(range(1, epochs + 1)), net.accuracies)
 
-def print_scatter(sizes, repeats, epochs, mini_batch_size, learning_rate):
-    for size in sizes:
-        initial = []
-        x = []
-        y = []
-        for i in range(repeats):
-            print(f"Repeat {i + 1} for {size}")
-            initial_new, x_new, y_new = extract_scatter(size, epochs, mini_batch_size, learning_rate)
-            initial.append(initial_new)
-            x = x + x_new
-            y = y + y_new
-            print(initial, x, y)
-        if True:
-            with open(f"data/{str(size)}.pkl", "wb") as f:
-                pickle.dump((initial, x, y), f)
-        plt.scatter(x, y, label=str(size))
-        
-    plt.title("Hyperparameter comparison")
-    plt.xlim((0, epochs + 1))
+def write_plot(size, repeats, epochs, mini_batch_size, learning_rate):
+    initial = []
+    x = []
+    y = []
+    for i in range(repeats):
+        print(f"Repeat {i + 1}/{repeats} for network structure {size}")
+        initial_new, x_new, y_new = extract_plot(size, epochs, mini_batch_size, learning_rate)
+        initial.append(initial_new)
+        x = x + x_new
+        y = y + y_new
+    if True:
+        with open(f"data/{str(size)}.pkl", "wb") as f:
+            pickle.dump((initial, x, y), f)
+
+def load_plot(structure):
+    with open(f"data/{str(structure)}.pkl", "rb") as f:
+        initial, x, y = pickle.load(f)
+
     plt.xticks(range(min(x), max(x) + 1))
+
+    x = [x[i:i + 10] for i in range(0, len(x), 10)]
+    y = [y[i:i + 10] for i in range(0, len(y), 10)]
+
+    for i in range(len(x)):
+        plt.plot(x[i], y[i])
+
+    plt.title(f"{str(structure)} network (10 runs)")
+    plt.xlim((0, 10 + 1))
     plt.grid(axis='x', linestyle='--', linewidth=0.5)
-    plt.legend(title="Network structure")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
     plt.show()
 
 if __name__ == "__main__":
-    print_scatter([[784, 10]], repeats=20, epochs=10, mini_batch_size=20, learning_rate=5)
+    write_plot([784, 30, 10], repeats=10, epochs=10, mini_batch_size=20, learning_rate=5)
+    load_plot([784, 30, 10])
